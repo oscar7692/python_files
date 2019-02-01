ee47f3f594a1d2ffd09290fe34334edd467a1ada
oscar7692 <oscar.acosta@alumnos.udg.mx>
2019-02-01 07:49:28 -0600

    new files were added

--- HelloWorld/HelloWorld/HelloWorld.py
+++
@@ -1,4 +0,0 @@
-print("hello world\n this message provides a sound alert\a")
-print('hello world')
-print("the backslash are a special character in python \"#")
-print("there is a extrange form to print on screen\t" + 'surprise mutherfucker')
