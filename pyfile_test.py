c7a6c309ba1fa5261622d7e1876cea46d05b18e7
op <op@localhost.localdomain>
2020-03-20 19:42:40 -0600

    somo additions

--- pyfile_test.py
+++ pyfile_test.py
@@ -7,14 +7,14 @@ b = 11.3
 c = 5
 d = 33.5
 #   suma
-print (a+b)
+print(a+b)
 #   resta
-print (c-a)
+print(c-a)
 #   multiplicacion
-print (d*c)
+print(d*c)
 #   exponente
-print (c**2)
+print(c**2)
 #   divicion
-print (float(c)/a)
+print(float(c)/a)
 #   modulo
-print (7 % 3)
+print(7 % 3)
